838a0d2199cf5f98a353684932f967a09700fbc1
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-28 13:22:23 -0400

    setup and deployment script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ sudo dnf install -y curl
 curl -fsSL https://bun.sh/install | bash
 curl -fsSL https://rpm.nodesource.com/setup_current.x | sudo bash -
 sudo dnf install -y nodejs
+sudo yum install cronie -y
+sudo systemctl enable crond.service
+sudo systemctl start crond.service
